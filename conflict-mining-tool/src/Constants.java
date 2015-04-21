bc709080c47b006e249e5dd3240ccfb4170ec1eb
marksuurland <marksuurland1@gmail.com>
2015-04-21 09:56:33 +0200

    Update constants

--- conflict-mining-tool/src/Constants.java
+++ conflict-mining-tool/src/Constants.java
@@ -1,74 +1,37 @@
 import java.io.File;
-import java.util.Vector;
 
 public class Constants {
 
-	public static String REPOSITORY_PREFIX = "";
+	public static String REPOSITORY_PREFIX = "C:\\Users\\Mark\\work\\" + File.separator;
+		
+	//System.out.println("Constants - Working path: " + REPOSITORY_PREFIX);	
 
-	static 
-	{
-		Vector<String> possiblePaths = new Vector<String>();
-		possiblePaths.add("C:\\Users\\Mark\\work\\"); // mark - work machine
-
-		for (String possiblePath : possiblePaths) {
-			if (new File(possiblePath).exists()) {
-				REPOSITORY_PREFIX = possiblePath;
-				break;
-			}
-		}
-		if (!REPOSITORY_PREFIX.endsWith(File.separator)) {
-			REPOSITORY_PREFIX += File.separator;
-		}
-
-		System.out.println("Constants - Working path: " + REPOSITORY_PREFIX);
-	}
-
-	private final static String jGit = "jgit";
+/*	private final static String jGit = "jgit";
 	private final static String voldemort = "voldemort";
 	private final static String test = "TestProject";
 	private final static String linux = "linux-2.6";
 	private final static String rails = "rails";
 	private final static String homebrew = "homebrew";
 	private final static String mangos = "mangos";
-	private final static String git = "git";
+	private final static String git = "git";*/
 	private final static String jquery = "jquery";
 
-	// Select the project you want to try out
+	// Repo for analysis
 	public static String PROJECT = jquery;
-
-	// don't change this one
 	public static String PROJECT_PATH = REPOSITORY_PREFIX + PROJECT + File.separator;
 
+	//Output files
 	public static String XML_TMP = REPOSITORY_PREFIX + PROJECT + "_out_tmp.xml";
 	public static String XML_FINAL = REPOSITORY_PREFIX + PROJECT + "_out_final.xml";
 	public static String DOT_FINAL = REPOSITORY_PREFIX + PROJECT + "_out_final.dot";
 
+	//Settings 
 	public static final boolean RECOVER_PREVIOUS = true;
-
 	public final static int POOL_SIZE = 1;
-
 	public final static int TMP_WRITE_FREQ = 100;
 
-	public static String GIT_PATH = "";
-
-	static {
-		String opt1 = "/usr/local/git/bin/git";
-		String opt2 = "/usr/bin/git";
-		String opt3 = "C:\\Users\\Mark\\AppData\\Local\\GitHub\\PortableGit_c2ba306e536fdf878271f7fe636a147ff37326ad\\bin\\git.exe";
-
-		File f = new File(opt1);
-		if (new File(opt1).exists()) {
-			GIT_PATH = opt1;
-		} else if (new File(opt2).exists()) {
-			GIT_PATH = opt2;
-		}
-		else if (new File(opt3).exists()) {
-			GIT_PATH = opt3;
-		} else {
-			throw new RuntimeException("Add another git path option clause");
-		}
-		System.out.println("Constants - Git install path: " + GIT_PATH);
-	}
+	//Git instal path
+	public static String GIT_PATH = "C:\\Users\\Mark\\AppData\\Local\\GitHub\\PortableGit_c2ba306e536fdf878271f7fe636a147ff37326ad\\bin\\git.exe";
 
 	public static void setProject(String project) {
 		System.out.println("Constants::setProject( " + project + " )");
